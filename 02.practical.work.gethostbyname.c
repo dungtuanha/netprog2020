9554b66b7695a6418a2915aa94920ba72646b13c
dungtuanha <dungha619@gmail.com>
2020-04-28 12:53:06 +0700

    Labwork 2

--- 02.practical.work.gethostbyname.c
+++ 02.practical.work.gethostbyname.c
@@ -5,13 +5,17 @@
 #include <netdb.h>
 extern int h_errno;
 
-int main(){
-	char hostname[100];
-	printf("Enter hostname: ");
-	scanf("%s", hostname);
+int main(int argc, char *argv[]){
+	while (argv[0] == NULL){
+		char hostname[100];
+		printf("Enter hostname: ");
+		scanf("%s", argv[0]);
+	}
 
-	struct hostent *ip_data = gethostbyname(hostname);
-	char *host_ip = inet_ntoa(*(struct in_addr*) ip_data->h_addr_list[0]);
-
-	printf("The ip of %s is: %s\n", hostname, host_ip);
+	struct hostent *ip_data = gethostbyname(argv[1]);
+	int n = sizeof(inet_ntoa(*(struct in_addr*) ip_data->h_addr_list));
+	for(int i = 0; i < n; i++){
+		char *host_ip = inet_ntoa(*(struct in_addr*) ip_data->h_addr_list[i]);
+		printf("The ip of %s is: %s\n", argv[1], host_ip);
+	}
 }
